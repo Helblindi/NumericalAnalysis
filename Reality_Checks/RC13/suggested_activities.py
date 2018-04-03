1c4fe5d196312a45ca0c6371cad3ab3d63b965a2
msheridan <mspencersheridan@gmail.com>
2018-04-02 21:29:07 -0600

    Finished gradient function and pushing some images from previous Reality Checks

--- Reality_Checks/RC13/suggested_activities.py
+++ Reality_Checks/RC13/suggested_activities.py
@@ -2,14 +2,16 @@ import numpy as np
 from get_potential_energy import *
 import scipy.optimize as opt
 from nelder_mead import *
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def sa1():
     """
-    Write a function that returns the potential energy U=∑i<j1ri j12-1ri j1
-    where ri_j is given at the top of p. 581. Apply Nelder–Mead to find the
+    Write a function that returns the potential energy U=∑i<j (1/r_ij^12 -1/r_ij^6)
+    where r_ij is given at the top of p. 581. Apply Nelder–Mead to find the
     minimum energy for n=5. Try several initial guesses until you are convinced
-    you have the absolute minimum. How many steps are required?To help you
+    you have the absolute minimum. How many steps are required? To help you
     check your potential function when n=5, here is one correct input, output
     pair. U(1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0) = -6.0102023319615911
     """
@@ -18,8 +20,8 @@ def sa1():
     z2 = 1
     v1 = np.array([0, 0, z2])
 
-    # ig = np.array([origin, v1, [1, 0, 0], [0, 1, 0], [1, 1, 0]])
-    ig = np.array([origin, v1])
+    ig = np.array([origin, v1, [1, 0, 0], [0, 1, 0], [1, 1, 0]])
+    # ig = np.array([origin, v1])
 
     # need to first convert the multidimensional array into a vector array
     m, n = np.shape(ig)
@@ -27,7 +29,11 @@ def sa1():
     ig = np.reshape(ig, x)
 
     # Chilton advised we should use scipy.optimize over his code
-    x_min = opt.minimize(get_potential_energy, ig, method='Nelder-Mead')
+    x_min = opt.minimize(U, ig, method='Nelder-Mead')
+
+    # check
+    print("Value should be -6.0102023319615911: ",
+          U([1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0]))
 
     print(x_min)
     return
@@ -41,6 +47,27 @@ def sa2():
     be three or four lines connecting each atom. You are welcome to use
     Python or Mathematica.
     """
+    # optimal node location for n = 5
+    # http://doye.chem.ox.ac.uk/jon/structures/LJ/tables.150.html
+    x = np.array([-0.26047, 0.26047, -0.41449, -0.19441, 0.60890])
+    y = np.array([0.73631, -0.73632, -0.36526, 0.28435, 0.08091])
+    z = np.array([0.47271, -0.47271, 0.34056, -0.55004, 0.20949])
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # plot each point
+    ax.scatter(x, y, z, 'bo')
+
+    # plot line connecting the points
+    # https://stackoverflow.com/questions/41563347/connecting-points-to-a-central-point-on-3d-scatter-python?rq=1
+    for a, b, c in zip(x, y, z):
+        for d, e, f in zip(x, y, z):
+            ax.plot3D([a, d], [b, e], [c, f], 'b-')
+
+    plt.title('Approximate Solution for $n=5$', fontsize=24)
+    plt.savefig('sa2_fig.png')
+    plt.show()
+
     return
 
 
@@ -55,6 +82,29 @@ def sa3():
     0.65625, 0.65625,0.65625,-1.3125, 0.79891,-1.45516, 0.79891,-1.3125,
     0.65625, 0.65625,-0.79891, 0.14266,-0.79891]
     """
+    # Check
+    print('Value should be: [0.65625, 0.0, 0.65625, 0.65625, '
+          '0.65625, -1.3125, 0.79891, -1.45516, 0.79891, -1.3125, '
+          '0.65625, 0.65625, -0.79891, 0.14266, -0.79891]')
+    print('Value is: ', grad_U([1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0]))
+
+    # fixed points in our model
+    origin = np.array([0, 0, 0])
+    z2 = 1
+    v1 = np.array([0, 0, z2])
+
+    ig = np.array([origin, v1, [1, 0, 0], [0, 1, 0], [1, 1, 0]])
+    # ig = np.array([origin, v1])
+
+    # need to first convert the multidimensional array into a vector array
+    m, n = np.shape(ig)
+    x = m * n
+    ig = np.reshape(ig, x)
+
+    # Chilton advised we should use scipy.optimize over his code
+    x_min = opt.minimize(U, ig, method='CG', jac=grad_U)
+
+    print(x_min)
     return
 
 
@@ -64,6 +114,23 @@ def sa4():
     of your potential function when n=5 using only the potential function itself(
     not the gradient). You cannot use Nelder-Mead for this.
     """
+    # fixed points in our model
+    origin = np.array([0, 0, 0])
+    z2 = 2
+    v1 = np.array([0, 0, z2])
+
+    ig = np.array([origin, v1, [1, 0, 0], [0, 1, 0], [1, 1, 0]])
+    # ig = np.array([origin, v1])
+
+    # need to first convert the multidimensional array into a vector array
+    m, n = np.shape(ig)
+    x = m * n
+    ig = np.reshape(ig, x)
+
+    # Chilton advised we should use scipy.optimize over his code
+    x_min = opt.minimize(U, ig, method='Powell')
+
+    print(x_min)
     return
 
 
@@ -72,6 +139,8 @@ def sa5():
     Apply the methods used in (1), (3), and (4) when n=6. Rank the methods
     according to reliability and efficiency.
     """
+    # QUESTION
+    # What do we start each variable in our nodes at?
     return
 
 
@@ -89,7 +158,8 @@ def sa7():
     """
     Determine and plot minimum-energy conformations for larger n. Information
     on minimum-energy Lennard-Jones clusters for n up to several hundred is posted
-    at the link provided in (b) above, so your answers can be readily checked. You
-    should do at least n=8.
+    at the link provided, so your answers can be readily checked. You should do at
+    least n=8.
+    http://doye.chem.ox.ac.uk/jon/structures/LJ/tables.150.html
     """
     return
